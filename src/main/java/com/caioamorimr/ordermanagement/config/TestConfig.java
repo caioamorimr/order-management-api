e13237568a81c96d44658907890672600bd91ba8
Caio Amorim <caioamorimribeiro@gmail.com>
2026-04-10 15:38:12 -0300

    Order Status enum

--- src/main/java/com/caioamorimr/ordermanagement/config/TestConfig.java
+++ src/main/java/com/caioamorimr/ordermanagement/config/TestConfig.java
@@ -2,6 +2,7 @@ package com.caioamorimr.ordermanagement.config;
 
 import com.caioamorimr.ordermanagement.entities.Order;
 import com.caioamorimr.ordermanagement.entities.User;
+import com.caioamorimr.ordermanagement.entities.enums.OrderStatus;
 import com.caioamorimr.ordermanagement.repositories.OrderRepository;
 import com.caioamorimr.ordermanagement.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,9 +28,9 @@ public class TestConfig implements CommandLineRunner {
         User u1 = new User(null, "Caio Amorim", "caio@email.com", "988888888", "123456");
         User u2 = new User(null, "Maria", "maria@email.com", "977777777", "123456");
 
-        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
-        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
-        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
+        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
+        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
+        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
 
         userRepository.saveAll(List.of(u1, u2));
         orderRepository.saveAll(List.of(o1, o2, o3));
---
+++ src/main/java/com/caioamorimr/ordermanagement/entities/enums/OrderStatus.java
@@ -0,0 +1,29 @@
+package com.caioamorimr.ordermanagement.entities.enums;
+
+public enum OrderStatus {
+
+    WAITING_PAYMENT(1),
+    PAID(2),
+    SHIPPED(3),
+    DELIVERED(4),
+    CANCELED(5);
+
+    private final int code;
+
+    private OrderStatus(int code) {
+        this.code = code;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public static OrderStatus valueOf(int code) {
+        for (OrderStatus status : OrderStatus.values()) {
+            if (status.getCode() == code) {
+                return status;
+            }
+        }
+        throw new IllegalArgumentException(String.format("Order status with code %d does not exist", code));
+    }
+}
