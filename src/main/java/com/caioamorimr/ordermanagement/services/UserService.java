d9f8892c18c9748186b89ca5edcc7d42a5249774
Caio Amorim <caioamorimribeiro@gmail.com>
2026-04-20 23:21:41 -0300

    Exception handling - update

--- src/main/java/com/caioamorimr/ordermanagement/services/UserService.java
+++ src/main/java/com/caioamorimr/ordermanagement/services/UserService.java
@@ -4,9 +4,9 @@ import com.caioamorimr.ordermanagement.entities.User;
 import com.caioamorimr.ordermanagement.repositories.UserRepository;
 import com.caioamorimr.ordermanagement.services.exceptions.DatabaseException;
 import com.caioamorimr.ordermanagement.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -42,9 +42,14 @@ public class UserService {
     }
 
     public User update(Long id, User user) {
-        User entity = userRepository.getReferenceById(id);
-        updateData(entity, user);
-        return userRepository.save(entity);
+        try {
+            User entity = userRepository.getReferenceById(id);
+            updateData(entity, user);
+            return userRepository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
+
     }
 
     private void updateData(User entity, User user) {
