ddcfba33a555b61f2a6406dfc51af958995da955
Caio Amorim <caioamorimribeiro@gmail.com>
2026-04-23 11:22:43 -0300

    PasswordEncoder in TestConfig

--- src/main/java/com/caioamorimr/ordermanagement/config/TestConfig.java
+++ src/main/java/com/caioamorimr/ordermanagement/config/TestConfig.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.math.BigDecimal;
 import java.time.Instant;
@@ -31,13 +32,15 @@ public class TestConfig implements CommandLineRunner {
     @Autowired
     private OrderItemRepository orderItemRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @Override
-    public void run(String... args) throws Exception {
+    public void run(String... args) {
 
         Category cat1 = new Category(null, "Electronics");
         Category cat2 = new Category(null, "Books");
         Category cat3 = new Category(null, "Computers");
-
         categoryRepository.saveAll(List.of(cat1, cat2, cat3));
 
         Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", new BigDecimal("90.5"), "");
@@ -52,30 +55,25 @@ public class TestConfig implements CommandLineRunner {
         p3.addCategory(cat3);
         p4.addCategory(cat3);
         p5.addCategory(cat2);
-
         productRepository.saveAll(List.of(p1, p2, p3, p4, p5));
 
-        User u1 = new User(null, "Caio Amorim", "caio@email.com", "988888888", "123456");
-        User u2 = new User(null, "Maria", "maria@email.com", "977777777", "123456");
+        User u1 = new User(null, "Caio Amorim", "caio@email.com", "988888888", passwordEncoder.encode("123456"));
+        User u2 = new User(null, "Maria", "maria@email.com", "977777777", passwordEncoder.encode("123456"));
+        userRepository.saveAll(List.of(u1, u2));
 
         Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
         Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
         Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
-
-        userRepository.saveAll(List.of(u1, u2));
         orderRepository.saveAll(List.of(o1, o2, o3));
 
         OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
         OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
         OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
         OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
-
         orderItemRepository.saveAll(List.of(oi1, oi2, oi3, oi4));
 
         Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
         o1.setPayment(pay1);
-
         orderRepository.save(o1);
-
     }
 }
