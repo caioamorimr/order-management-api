3e69d5c75826aaaaabad230cb921c9d28757f2f7
Caio Amorim <caioamorimribeiro@gmail.com>
2026-05-07 15:22:58 -0300

    Replace @MockBean to @MockitoBean

--- src/main/java/com/caioamorimr/ordermanagement/OrderManagementApiApplication.java
+++ src/main/java/com/caioamorimr/ordermanagement/OrderManagementApiApplication.java
@@ -2,8 +2,10 @@ package com.caioamorimr.ordermanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class OrderManagementApiApplication {
 
 	public static void main(String[] args) {
