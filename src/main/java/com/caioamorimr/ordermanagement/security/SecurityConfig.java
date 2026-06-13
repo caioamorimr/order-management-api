b31774a7f600492903fd30445df40d111e3d5fc2
Caio Amorim <caioamorimribeiro@gmail.com>
2026-06-13 10:49:44 -0300

    fix: add exceptionHandling to SecurityConfig to return 401

--- src/main/java/com/caioamorimr/ordermanagement/security/SecurityConfig.java
+++ src/main/java/com/caioamorimr/ordermanagement/security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.caioamorimr.ordermanagement.security;
 
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -49,7 +50,12 @@ public class SecurityConfig {
                                 .anyRequest().authenticated()
                 )
                 .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin))
-                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .exceptionHandling(ex -> ex
+                        .authenticationEntryPoint(
+                                (req, res, e) -> res.sendError(HttpServletResponse.SC_UNAUTHORIZED)
+                        )
+                );
 
         return http.build();
     }
